ed5860f895dced92f3fb12c437e3c43dee94a97d
Samuel Carrington <samuelcarrington@gmail.com>
2016-02-03 21:11:29 -0500

    commit

---
+++ GradeApplication/Controllers/GradesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GradeApplication.Models;
+
+namespace GradeApplication.Controllers
+{
+    public class GradesController : Controller
+    {
+        private GradesDBEntities db = new GradesDBEntities();
+
+        // GET: Grades
+        public ActionResult Index()
+        {
+            var grades = db.Grades.Include(g => g.Assignment).Include(g => g.Student);
+            return View(grades.ToList());
+        }
+
+        // GET: Grades/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Grade grade = db.Grades.Find(id);
+            if (grade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(grade);
+        }
+
+        // GET: Grades/Create
+        public ActionResult Create()
+        {
+            ViewBag.AssignmentID = new SelectList(db.Assignments, "AssignmentID", "Type");
+            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "FirstName");
+            return View();
+        }
+
+        // POST: Grades/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "GradeID,StudentID,AssignmentID,DateGraded")] Grade grade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Grades.Add(grade);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AssignmentID = new SelectList(db.Assignments, "AssignmentID", "Type", grade.AssignmentID);
+            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "FirstName", grade.StudentID);
+            return View(grade);
+        }
+
+        // GET: Grades/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Grade grade = db.Grades.Find(id);
+            if (grade == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AssignmentID = new SelectList(db.Assignments, "AssignmentID", "Type", grade.AssignmentID);
+            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "FirstName", grade.StudentID);
+            return View(grade);
+        }
+
+        // POST: Grades/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "GradeID,StudentID,AssignmentID,DateGraded")] Grade grade)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(grade).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AssignmentID = new SelectList(db.Assignments, "AssignmentID", "Type", grade.AssignmentID);
+            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "FirstName", grade.StudentID);
+            return View(grade);
+        }
+
+        // GET: Grades/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Grade grade = db.Grades.Find(id);
+            if (grade == null)
+            {
+                return HttpNotFound();
+            }
+            return View(grade);
+        }
+
+        // POST: Grades/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Grade grade = db.Grades.Find(id);
+            db.Grades.Remove(grade);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
